9c6345bb4e3368566ea64aeacbc6c94c508f35ac
GreenMachine\Robin <rdonatello@csuchico.edu>
2019-01-09 22:53:21 -0800

    start rebuild for sp19

--- _render_all_wks.R
+++ _render_all_wks.R
@@ -1,5 +1,5 @@
 rmarkdown::render("schedule.Rmd")
-# rmarkdown::render("wk/wk01.Rmd")
+rmarkdown::render("wk/wk01.Rmd")
 # rmarkdown::render("wk/wk02.Rmd")
 # rmarkdown::render("wk/wk03.Rmd")
 # rmarkdown::render("wk/wk04.Rmd")
